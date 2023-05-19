99e72bec3dedee9893e681f385bd3779b34cc7db
Suelen Magalhães <117091949+DevSuelen@users.noreply.github.com>
2023-05-18 21:14:52 -0300

    editando o javascript

--- aula_03/exercicio2.js
+++ aula_03/exercicio2.js
@@ -1,2 +1,23 @@
-let teste = selectTemp.value
-console.log(teste)
+divC.style.display = 'none'
+
+const hideInputs = () => {
+    let selected = selected.value
+    console.log(selected);
+
+    if(selected === 'c'){
+        divC.style.display = 'none'
+        divF.style.display = 'block'
+    }else if(selected === 'f'){
+        divC.style.display = 'none'
+        divF.style.display = 'block'
+    }
+}
+
+const toC = (e) => {
+    event.preventDefault()
+    console.log('para celsius')
+}
+const toC = (e) => {
+    event.preventDefault()
+    console.log('para fahrenheit')
+}
